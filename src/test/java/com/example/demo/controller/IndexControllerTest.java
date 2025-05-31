fb5750fbee602f6b7051e39f30c8f17f88e5aef8
kazuki178 <yahashi.kazuki0521@icloud.com>
2025-05-31 19:19:18 +0900

    リファクタリング

---
+++ src/test/java/com/example/demo/controller/IndexControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class IndexControllerTest {
+    @Test
+    void testDetailInfo() {
+
+    }
+
+    @Test
+    void testIndex() {
+
+    }
+}
