161bbc12d3ac7694ce9327f9a4de01cf6ded6b9d
kazuki178 <yahashi.kazuki0521@icloud.com>
2025-06-01 18:04:35 +0900

    test作成途中

--- src/test/java/com/example/demo/controller/IndexControllerTest.java
+++ src/test/java/com/example/demo/controller/IndexControllerTest.java
@@ -1,15 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void testDetailInfo() {
-
-    }
-
-    @Test
-    void testIndex() {
-
+    void testIndex() throws Exception {
+        mockMvc.perform(get("/detailInfo").param("id", "1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("detailInfo"));
     }
 }
