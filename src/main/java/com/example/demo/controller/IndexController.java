05c60fcde9472f1829b1c8c0106287663170bd1e
kazuki178 <yahashi.kazuki0521@icloud.com>
2025-06-14 09:56:56 +0900

    メッセージ修正

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,17 +1,16 @@
 package com.example.demo.controller;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.example.demo.model.GroupSchedule;
+import com.example.demo.service.GroupScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.model.GroupSchedule;
-import com.example.demo.service.GroupScheduleService;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -34,7 +33,7 @@ public class IndexController {
 
 		List<GroupSchedule> ScheduleList = new ArrayList<>();
 
-		// 1日開始で作成されるように処理
+		// 1日から開始で作成されるように処理
 		String title = null;
 		String group = null;
 		if (date == null) {
